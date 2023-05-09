66ea991af5bceb74c22bbc43ea0d27ec4e0a3aee
salrealTeknet <salihuabduljawwad.a@gmail.com>
2023-05-09 20:54:48 +0000

    correction update

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import pymongo
+
+
+def schools_by_topic(mongo_collection, topic: str):
+    """ Search the school based in school
+
+        Args:
+            mongo_collection: Collection
+            topic: Content
+
+        Return:
+            List of school
+    """
+    query: dict = {"topics": topic}
+    schools: list = []
+
+    for school in mongo_collection.find(query):
+        schools.append(school)
+
+    return schools
