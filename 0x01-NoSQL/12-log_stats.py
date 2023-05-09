b25a4c5e64a1d312910b7d2581707ccffe6de151
salrealTeknet <salihuabduljawwad.a@gmail.com>
2023-05-09 23:36:20 +0000

    addcorectn git push

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,22 +1,19 @@
 #!/usr/bin/env python3
+"""Print info f nginx logs """
 from pymongo import MongoClient
 
-
 if __name__ == "__main__":
+    """ check for all elements in a collection """
     client = MongoClient('mongodb://127.0.0.1:27017')
-    db_nginx = client.logs.nginx
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    collection = client.logs.nginx
 
-    count_logs = db_nginx.count_documents({})
-    print(f'{count_logs} logs')
+    print(f"{collection.estimated_document_count()} logs")
 
-    print('Methods:')
-    for method in methods:
-        count_method = db_nginx.count_documents({'method': method})
-        print(f'\tmethod {method}: {count_method}')
+    print("Methods:")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        method_count = collection.count_documents({'method': method})
+        print(f"\tmethod {method}: {method_count}")
 
-    check = db_nginx.count_documents(
-        {"method": "GET", "path": "/status"}
-    )
-
-    print(f'{check} status check')
+    check_get = collection.count_documents(
+        {'method': 'GET', 'path': "/status"})
+    print(f"{check_get} status check")
